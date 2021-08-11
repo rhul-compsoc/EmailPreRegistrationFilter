c7fd251d4cc39174b1019d972109adcab5888861
Gabriel Flores <gabriel.flores.m@ug.uchile.cl>
2021-08-11 06:23:00 -0400

    uploadin'

---
+++ README.md
@@ -0,0 +1,2 @@
+# EmailRegistrationFilter
+See the Extension page on [MediaWiki](https://www.mediawiki.org/wiki/Extension:EmailRegistrationFilter).
---
+++ includes/EmailFilterPasswordPrimaryAuthenticationProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+use MediaWiki\Auth\LocalPasswordPrimaryAuthenticationProvider;
+use MediaWiki\Auth\AuthenticationRequest;
+use MediaWiki\Auth\UserDataAuthenticationRequest;
+use MediaWiki\Auth\AuthenticationResponse;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Status;
+
+class EmailFilterPasswordPrimaryAuthenticationProvider
+	extends LocalPasswordPrimaryAuthenticationProvider {
+
+	public function testForAccountCreation( $user, $creator, array $reqs ) {
+		$config = MediaWiki\MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'EmailRegistrationFilter' );
+		$patterns = $config->get( 'PatternList' );
+		$is_whitelist = $config->get( 'IsWhiteList' );
+		$error_msg = $config->get( 'ErrorMessage' );
+
+		$userdata_req = AuthenticationRequest::getRequestByClass( $reqs, UserDataAuthenticationRequest::class );
+		if ( $userdata_req->email == null ) {
+			/* Nothing to do on no email. Might want to set $wgEmailConfirmToEdit=true;
+			 * or something like that */
+			return parent::testForAccountCreation($user, $creator, $reqs);
+		}
+
+		$email = $userdata_req->email;
+		$ret = StatusValue::newGood();
+		foreach ($patterns as $pattern) {
+			if (preg_match($pattern, $email)) {
+				if ( !$is_whitelist ){
+					$ret->fatal($error_msg);
+					return $ret;
+				}
+			}
+		}
+		// No matches
+		if ($is_whitelist) {
+			$ret->fatal($error_msg);
+			return $ret;
+		}
+
+		return parent::testForAccountCreation($user, $creator, $reqs);
+	}
+}
